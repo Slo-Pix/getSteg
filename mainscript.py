7e1f82a421d2fac4e3530fb7ef2af0861add5f28
Slo-Pix <shubham040605@gmail.com>
2025-05-13 16:45:17 +0530

    encoding files into pdf

--- mainscript.py
+++ mainscript.py
@@ -3,10 +3,12 @@ from steg_classes.pdf_steg import PdfSteg
 from steg_classes.image_steg import Imagesteg
 from steg_classes.audio_steg import audioSteg
 
-def encrypt(type, input_file, output_file, key, message, parser):
+def encrypt(type, input_file, output_file, key, message, parser, hidden_file):
     if type=="pdf":
-        if key:
+        if key and message:
             PdfSteg.encrypt(pdf_path=input_file, key=key, message=message, output_file_path=output_file)
+        elif key and hidden_file:
+            PdfSteg.encrypt_file(input_file=input_file,  key=key, hidden_file=hidden_file, output_file=output_file)
         else:
             parser.error("PDF requires -k option")
     elif type =="image":
@@ -17,8 +19,11 @@ def encrypt(type, input_file, output_file, key, message, parser):
         parser.error("Accepted inputs for -t include image, pdf and audio")
 
 
-def decrypt(type, input_file, parser):
-    if type=="pdf":
+def decrypt(type, input_file, parser, output_file, key):
+    if type=="pdf" and key and output_file:
+        PdfSteg.decrypt_file(input_file=input_file, output_file=output_file, key=key)
+        
+    elif type=="pdf":
         PdfSteg.decrypt(pdf_path=input_file)
     elif type=="image":
         Imagesteg.decrypt(input_file=input_file)
@@ -34,11 +39,12 @@ def main():
 
     parser.add_argument('-d', action='store_true', help='decrypt the file (requires -f option)')
     parser.add_argument('-e', action='store_true', help='encrypt the file (requires -f -m -o options)')
-    parser.add_argument('-f', required=False, type=str, help='path to the input file', metavar='INPUT_FILE')
+    parser.add_argument('-f', required=True, type=str, help='path to the input file', metavar='INPUT_FILE')
     parser.add_argument('-o', required=False, type=str, help='path to the output file', metavar='OUTPUT_FILE')
     parser.add_argument('-m', required=False, type=str, help='message you want to add (use "")', metavar='MESSAGE')
     parser.add_argument('-t', required=True, type=str, help='type of steg you want to perform', metavar='TYPE')
     parser.add_argument('-k', required=False, type=str, help='metadata key', metavar='KEY')
+    parser.add_argument('-hf',required=False,type=str,help='path to hide a file', metavar='HIDE_FILE' )
     args = parser.parse_args()
 
     if args.e and args.d:
@@ -51,13 +57,13 @@ def main():
         if not args.f:
             parser.error("must include -f option")
         else:
-            decrypt(type=args.t, input_file=args.f, parser=parser)
+            decrypt(type=args.t, input_file=args.f, parser=parser, output_file=args.o, key=args.k)
 
     elif args.e:
-        if (args.f and args.o and args.m and args.k) or (args.f and args.o and args.m):
-            encrypt(type=args.t, input_file=args.f,output_file=args.o,key=args.k, message=args.m, parser=parser)
+        if (args.o and (args.m or args.hf) and args.k) or (args.o and (args.m or args.hf)):
+            encrypt(type=args.t, input_file=args.f,output_file=args.o,key=args.k, message=args.m, parser=parser, hidden_file=args.hf)
         else:
-            parser.error("must include -f -m -o options (for image and audio) and an additional option -k for pdf")
+            parser.error("must include -f (-m or -hf) -o options (for image and audio) and an additional option -k for pdf")
 
 
 
--- steg_classes/pdf_steg.py
+++ steg_classes/pdf_steg.py
@@ -1,6 +1,64 @@
 import PyPDF2
 
+def save_file(binary_str, output_file):
+    try:
+        if len(binary_str) % 8 != 0:
+            raise ValueError("Binary string length must be 8")
+        
+        byte_chunks=[]
+        for i in range(0, len(binary_str), 8):
+            byte_chunks.append(binary_str[i:i+8])
+
+        byte_list=[]
+        for byte_chunk in byte_chunks:
+            byte_value=int(byte_chunk,2)
+            byte_list.append(byte_value)
+        
+        byte_list=bytes(byte_list)
+        with open(output_file, 'wb') as file:
+            file.write(byte_list)
+
+    except Exception as e:
+        print(f"An error occured : {e}")
+
+
 class PdfSteg:
+
+    def encrypt_file(input_file, key, hidden_file, output_file):
+        with open(input_file, 'rb') as file:
+            reader=PyPDF2.PdfReader(file)
+            writer=PyPDF2.PdfWriter()
+            writer.append_pages_from_reader(reader)
+
+            binary_list=[]
+            with open(hidden_file, 'rb') as hf:
+                file_bytes=hf.read()
+                for byte in file_bytes:
+                    binary_list.append(format(byte, '08b'))
+
+            message = ''.join(binary_list) 
+
+            metadata=reader.metadata
+            metadata.update({key : message})
+            writer.add_metadata(metadata)
+            with open(output_file, 'wb') as output:
+                writer.write(output)
+        print("\n -- file creation successful --") 
+    
+
+    def decrypt_file(input_file, output_file, key):
+        with open(input_file, 'rb') as file:
+            reader= PyPDF2.PdfReader(file)
+            metadata=reader.metadata
+            flag=False
+            for item in metadata:
+                if item == key:
+                    flag=True
+                    save_file(binary_str=metadata[item], output_file=output_file)
+            if flag==False:
+                print("No such key name exists")
+
+
     def decrypt(pdf_path):
         with open(pdf_path, 'rb') as file:
             reader = PyPDF2.PdfReader(file) 
